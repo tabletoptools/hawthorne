aa811eb590b43c40592317d741affb5f4a5e2eb3
Carlo Field <cfi@bluesky-it.ch>
2018-05-22 14:28:49 +0200

    Update API Base

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,5 +6,5 @@ export const environment = {
     persistence: PersistenceType.LOCAL,
     version: version,
     loginURL: "https://discordapp.com/api/oauth2/authorize?client_id=406607705952747530&redirect_uri=https%3A%2F%2Ftabletoptools.github.io%2Fhawthorne%2Fcallback.html&response_type=token&scope=identify%20guilds%20email",
-    serverBase: "https://api.dnapolian.com"
+    serverBase: "https://api.dnapolian.com/api"
 };
